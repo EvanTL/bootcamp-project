0f691bb36a1e65441ff9ea5eef11da3255e109bb
EvanTL <evantanakaliman@gmail.com>
2023-10-04 16:51:48 +0800

    Review map moved left, remove grid on small screen

--- src/components/ProductReviews.js
+++ src/components/ProductReviews.js
@@ -20,7 +20,24 @@ const ProductReviews = ({reviews, loading, error}) => {
     return(
         <div className='reviews bg-[#6499E9] p-3 my-3 rounded-lg'>
         <h3 className=' text-center'>Reviews</h3>
-        <div className='grid grid-cols-2 gap-3'>
+        <div className='lg:grid lg:grid-cols-2 gap-3'>
+            <div>
+                <p>Write a review</p>
+                <div className='bg-white rounded-lg p-2'>
+                    <h5 className='font-semibold'>Rating:</h5>
+                    <select value={stars} onChange={updateStars} className='bg-gray-200 w-full'>
+                        <option value={0}>Select...</option>
+                        <option value={1}>1 Star</option>
+                        <option value={2}>2 Stars</option>
+                        <option value={3}>3 Stars</option>
+                        <option value={4}>4 Stars</option>
+                        <option value={5}>5 Stars</option>
+                    </select>
+                    <h5 className='font-semibold mt-3'>Comment:</h5>
+                    <textarea className='w-full p-2 border-2 border-black rounded-lg resize-none'/>
+                    <button className='btn'>Submit Review</button>
+                </div>
+            </div>
             <div>
                 <p>{reviews.length} customer {reviews.length > 1 ? <>reviews</> : <>review</>}</p>
                 {reviews.length > 0 && reviews.map(content => {
@@ -36,23 +53,6 @@ const ProductReviews = ({reviews, loading, error}) => {
                     )
                 })}
                 {reviews.length === 0 && <h5>No reviews yet</h5>}
-            </div>
-            <div>
-                <p>Write a review</p>
-                <div className='bg-white rounded-lg p-2'>
-                <h5 className='font-semibold'>Rating:</h5>
-                    <select value={stars} onChange={updateStars} className='bg-gray-200 w-full'>
-                        <option value={0}>Select...</option>
-                        <option value={1}>1 Star</option>
-                        <option value={2}>2 Stars</option>
-                        <option value={3}>3 Stars</option>
-                        <option value={4}>4 Stars</option>
-                        <option value={5}>5 Stars</option>
-                    </select>
-                <h5 className='font-semibold mt-3'>Comment:</h5>
-                <textarea className='w-full p-2 border-2 border-black rounded-lg resize-none'/>
-                <button className='btn'>Submit Review</button>
-                </div>
             </div>
         </div>
         </div>
