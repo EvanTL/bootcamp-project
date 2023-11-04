184ef20c0a5e7b3ba7eb4fea19b16130f8becb5c
EvanTL <evantanakaliman@gmail.com>
2023-11-04 16:21:19 +0800

    z-index changes

--- src/components/PageHero.js
+++ src/components/PageHero.js
@@ -5,7 +5,7 @@ import './pagehero.css'
 
 const PageHero = ({ title, product }) => {
   return (
-    <div className='section-hero fixed top-[5rem] mb-3'>
+    <div className='section-hero fixed top-[5rem] mb-3 z-10'>
       <div className={styled}>
         <h3 className='ml-[5rem] mb-0 font-bold'>
           <Link to='/'>Home </Link>
