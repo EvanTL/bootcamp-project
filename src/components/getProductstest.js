22cf09f76f45984b07b24982b6bc411e65b8be1e
EvanTL <evantanakaliman@gmail.com>
2023-11-04 17:22:31 +0800

    Added auth but commented

--- src/components/getProductstest.js
+++ src/components/getProductstest.js
@@ -1,5 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import Loading from './Loading';
+import PageHero from './PageHero';
+import ProductImages from './ProductImages';
+import { formatPrice } from '../utils/helpers';
+import { Link } from 'react-router-dom';
 
 export default function GetProducts(){
     const [product, setProduct] = useState(null)
@@ -9,6 +13,9 @@ export default function GetProducts(){
     async function fetchData() {
         await fetch('http://localhost:8000/shop/product/65424e6b05ae1764ef3710e8',{
             crossDomain: true,
+            // headers: {
+            //     Authorization: "Bearer " + this.props.token
+            // }
         })
         .then(response => response.json())
         .then(data => {
@@ -28,11 +35,39 @@ export default function GetProducts(){
 
     useEffect(() => {
         fetchData()
-    })
+    }, [])
 
     if(loading){
         return <Loading/>
     }
 
+    const {
+        title,
+        price,
+        description,
+        company,
+        imageUrl,
+        colors
+      } = product;
     
+      return (
+        <>
+        <div className='mt-20'>
+        <PageHero title={title} product='products'/>
+        <div className='pt-[5rem] mx-[174px]'>
+          <div className='lg:grid lg:grid-cols-2 gap-5'>
+            <section>
+            <Link to='/products' className='w-fit float-left m-auto text-center bg-[#676767] text-white rounded-[var(--radius)]
+            text-[0.875rem] px-3 py-[6px] mb-2 uppercase hover:opacity-50 transition-[var(--transition)]'>Back to Products</Link>
+            </section>
+            <section className='description mt-[35px]'>
+              <h2 className='text-[#071952] mb-3'>{title}</h2>
+              <h5 className='text-[var(--clr-grey-5)]'>{formatPrice(price)}</h5>
+              <p>{description}</p>
+            </section>
+          </div>
+        </div>
+        </div>
+        </>
+      );
 }
