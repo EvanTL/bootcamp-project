a2a1c067c51c174db140cc3fc7a36de45c9c5df2
EvanTL <evantanakaliman@gmail.com>
2023-11-07 22:21:56 +0800

    Removed useAuth0 (Unused)

--- src/pages/PrivateRoute.js
+++ src/pages/PrivateRoute.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
-import { useAuth0 } from '@auth0/auth0-react';
 
 const PrivateRoute = ({ children }) => {
   const token = localStorage.getItem("userInfo");
