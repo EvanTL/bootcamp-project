5309d0f920dd51f8b738babad349b5369b972e9e
EvanTL <evantanakaliman@gmail.com>
2023-11-18 23:24:34 +0800

    Integration for product update

--- Web-admin/src/pages/dashboard/ProductEditScreen.jsx
+++ Web-admin/src/pages/dashboard/ProductEditScreen.jsx
@@ -8,37 +8,69 @@ import {
     Select,
     Option,
   } from "@material-tailwind/react";
+import { useProductsContext } from "@/context/products_context";
   
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export function ProductEditScreen() {
+  const productId = window.location.pathname.split('/')[3]
+  const { fetchSingleProduct, updateSingleProduct, single_product, update_data } = useProductsContext()
+  const navigate = useNavigate()
 
-  const [file, setFile] = useState();
+  useEffect(() => {
+    fetchSingleProduct(productId)
+  }, [])
+
+  const {title, price, description} = single_product
+
+  const [image, setImage] = useState();
+  const [name, setName] = useState(title)
+  const [newPrice, setNewPrice] = useState(price)
+  const [desc, setDesc] = useState(description)
     function handleChange(e) {
-        console.log(e.target.files);
-        setFile(URL.createObjectURL(e.target.files[0]));
+      console.log(image)
+      setImage(URL.createObjectURL(e.target.files[0]));
+    }
+
+    const handleSubmit = async (e) => {
+      e.preventDefault()
+      await updateSingleProduct(name, newPrice, desc, image, productId)
+      console.log(update_data)
+      if (update_data.status === 200){
+        navigate('/dashboard/products')
+      }
     }
 
     return (
       
       <Card color="transparent" shadow={false}>
         <Typography variant="h4" color="blue-gray">
-          Edit Product
+          Edit Product: {title}
         </Typography>
-        <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
-          <div className="mb-4 flex flex-col gap-6">
-            <Input size="lg" label="Name" />
-            <Input size="lg" label="Price" />
+        <form onSubmit={handleSubmit} className="mt-8 mb-2 w-80 sm:w-full">
+          <div className="mb-4 grid grid-cols-2 gap-6">
+            <div>
+            <Input size="lg" label="Name" value={name} onChange={(e) => {
+              setName(e.target.value)
+            }} />
             <Select label="Category">
               <Option>Celana</Option>
               <Option>Topi</Option>
               <Option>T-Shirt</Option>
             </Select>
-            <Textarea  size="lg" color="purple" label="Description" />
-            <input type="file" onChange={handleChange} />
-            <img className="h-45 w-full rounded-lg object-cover object-center" src={file} />
+            <Textarea  size="lg" color="purple" label="Description" value={desc} onChange={(e) => {
+            }} />
+            <input type="file" className="mt-2" onChange={handleChange} />
+            <img className="h-45 w-full rounded-lg object-cover object-center" src={image} />
+            </div>
+            <div>
+            <Input size="lg" label="Price" value={newPrice} onChange={(e) => {
+              console.log("Price:" + newPrice)
+            }} />
+            </div>
           </div>
-          <Button className="mt-6" fullWidth>
+          <Button className="mt-6" fullWidth type="submit">
             Update
           </Button>
         </form>
