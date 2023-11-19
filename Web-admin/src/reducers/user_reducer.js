17a679d6304d90b5aa98f10e3920992752de9612
EvanTL <evantanakaliman@gmail.com>
2023-11-19 22:55:10 +0800

    Reducer for delete user

--- Web-admin/src/reducers/user_reducer.js
+++ Web-admin/src/reducers/user_reducer.js
@@ -7,6 +7,9 @@
   GET_SINGLE_USER_BEGIN,
   GET_SINGLE_USER_SUCCESS,
   GET_SINGLE_USER_ERROR,
+  DELETE_USERS_BEGIN,
+  DELETE_USERS_SUCCESS,
+  DELETE_USERS_ERROR
 } from '../components/actions'
 
 const user_reducer = (state, action) => {
@@ -49,6 +52,27 @@ const user_reducer = (state, action) => {
       ...state,
       single_user_loading: false,
       single_user_error: true,
+    }
+  }
+  if (action.type === DELETE_USERS_BEGIN) {
+    return {
+      state,
+      users_loading: true
+    }
+  }
+  if (action.type === DELETE_USERS_SUCCESS) {
+    return {
+      state,
+      users_loading: false,
+      update_data: action.payload
+    }
+  }
+  if (action.type === DELETE_USERS_ERROR) {
+    return {
+      state,
+      users_loading: false,
+      users_error: true,
+      update_data: action.payload
     }
   }
   throw new Error(`No Matching "${action.type}" - action type`)
