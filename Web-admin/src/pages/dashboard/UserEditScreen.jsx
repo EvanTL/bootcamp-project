3f6756c293bf6b9ed083ff3622ff480423fb7c86
EvanTL <evantanakaliman@gmail.com>
2023-11-20 23:12:23 +0800

    Changes for edit user

--- Web-admin/src/pages/dashboard/UserEditScreen.jsx
+++ Web-admin/src/pages/dashboard/UserEditScreen.jsx
@@ -15,39 +15,24 @@ import { useUsersContext } from "@/context/user_context";
 
 export function UserEditScreen() {
   const userId = window.location.pathname.split('/')[3]
-  const { fetchSingleUser } = useUsersContext()
+  const { fetchSingleUser, single_user, single_user_loading, updateUser, update_data } = useUsersContext()
   const navigate = useNavigate()
-
   useEffect(() => {
-    fetchSingleProduct(userId)
+    fetchSingleUser(userId)
+    console.log(single_user)
   }, [])
 
-  const {title} = single_product
-
-  const [image, setImage] = useState(null);
-  const [name, setName] = useState(title)
-  const [newPrice, setNewPrice] = useState(single_product.price)
-  const [desc, setDesc] = useState(single_product.description)
-  const [featured, setFeatured] = useState(single_product.featured)
-    function handleChange(e) {
-      console.log(image)
-      setImage(e.target.files[0]);
-    }
+  const [name, setName] = useState(single_user.name)
+  const [email, setEmail] = useState(single_user.email)
 
     const handleSubmit = async (e) => {
       e.preventDefault()
-      const formData = new FormData()
-      formData.append('image', image)
-      formData.append('newTitle', name)
-      formData.append('newPrice', newPrice)
-      formData.append('newDesc', desc)
-      formData.append('newFeatured', featured)
 
-      await updateSingleProduct(formData, productId)
-      if(JSON.stringify(update_data) !== "{}"){
+      await updateUser(userId, name, email)
+      if(update_data){
         alert(update_data.message)
         if (update_data.status === 200){
-          navigate('/dashboard/products')
+          navigate('/dashboard/users')
         }
       }
     }
@@ -56,31 +41,20 @@ export function UserEditScreen() {
       
       <Card color="transparent" shadow={false}>
         <Typography variant="h4" color="blue-gray">
-          Edit User: {title}
+          Edit User: {single_user.name}
         </Typography>
-        <form onSubmit={handleSubmit} className="mt-8 mb-2 w-80 sm:w-full">
+
+        {!single_user_loading && (
+          <form onSubmit={handleSubmit} className="mt-8 mb-2 w-80 sm:w-full">
           <div className="mb-4 grid grid-cols-2 gap-6">
             <div>
             <Input size="lg" label="Name" value={name} onChange={(e) => {
               setName(e.target.value)
             }} />
-            <Select label="Category">
-              <Option>Celana</Option>
-              <Option>Topi</Option>
-              <Option>T-Shirt</Option>
-            </Select>
-            <Textarea  size="lg" color="purple" label="Description" value={desc} onChange={(e) => {
-              setDesc(e.target.value)
-            }} />
-            <input type="file" className="mt-2" onChange={handleChange} />
-            <img src={image} className="h-45 w-full rounded-lg object-cover object-center" alt="not found" />
             </div>
             <div>
-            <Input size="lg" label="Price" value={newPrice} onChange={(e) => {
-              setNewPrice(e.target.value)
-            }} />
-            <Checkbox size='md' label="featured" value={featured} onChange={(e) => {
-              setFeatured(!featured)
+            <Input size="lg" label="Email" value={email} onChange={(e) => {
+              setEmail(e.target.value)
             }} />
             </div>
           </div>
@@ -88,6 +62,8 @@ export function UserEditScreen() {
             Update
           </Button>
         </form>
+        )}
+        
       </Card>
     );
   }
