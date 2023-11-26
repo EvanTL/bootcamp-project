7fc0cb59f9fb769d4abaa92cfbba25daf41b02d3
EvanTL <evantanakaliman@gmail.com>
2023-11-26 20:18:14 +0800

    New reducers: Get order

--- React-fe/src/reducers/order_reducer.js
+++ React-fe/src/reducers/order_reducer.js
@@ -19,6 +19,34 @@ const order_reducer = (state, action) => {
         
         return {...state, loading: false, error: action.payload}
     }
+
+    if (action.type === 'GET_ORDERS_REQUEST') {
+        return {...state, loading: true}
+    }
+
+    if (action.type === 'GET_ORDERS_SUCCESS') {
+    
+        return {...state, loading: false, orders: action.payload}
+    }
+
+    if (action.type === 'GET_ORDERS_FAIL') {
+        
+        return {...state, loading: false, error: action.payload}
+    }
+
+    if (action.type === 'GET_SINGLE_ORDER_REQUEST') {
+        return {...state, loading: true}
+    }
+
+    if (action.type === 'GET_SINGLE_ORDER_SUCCESS') {
+    
+        return {...state, loading: false, single_order: action.payload}
+    }
+
+    if (action.type === 'GET_SINGLE_ORDER_FAIL') {
+        
+        return {...state, loading: false, error: action.payload}
+    }
     
     
 }
